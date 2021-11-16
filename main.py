7b15b2b54f4ab7b0413fb5171148097f91a307f6
prisha12341 <93686097+prisha1234@users.noreply.github.com>
2021-11-16 00:30:10 +0000

    ...

---
+++ main.py
@@ -0,0 +1,4 @@
+print('this is a multiplication table generator program.')
+n = int (input ("enter a  number to see its full table (0-10)"))
+for i in range (1,11):
+ print (n, " x ", i," = ", i *n)
